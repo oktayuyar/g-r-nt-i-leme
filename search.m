3f0557f7e497b8cff3bdb9f6e18955a9674fe5fd
oktayuyar <oktay-uyar@hotmail.com>
2016-05-14 19:44:22 +0300

    nesnelerin sayılması

---
+++ search.m
@@ -0,0 +1,17 @@
+function [ buldumu,x,y ] = search( Matris,a,b )
+%FİND Summary of this function goes here
+%   Detailed explanation goes here
+
+[r,c]=size(Matris);
+for t=1:20
+    x=ceil(t*cos(t)/10+a);
+    y=ceil(t*sin(t)/10+b);
+    if x<=r && y<=c && x>0 && y>0
+        if Matris(x,y)==1
+            buldumu=1;
+            return;
+        end
+    end
+end
+buldumu=0;
+end
